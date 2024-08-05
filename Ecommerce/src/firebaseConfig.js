66109bf25f794367a71784f43cdf8b3cc8ca9689
Ismael267 <77760562+Ismael267@users.noreply.github.com>
2024-08-05 22:02:19 +0000

    Update firebaseConfig.js

--- Ecommerce/src/firebaseConfig.js
+++ Ecommerce/src/firebaseConfig.js
@@ -9,14 +9,14 @@ import { getFirestore} from 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB1hZkGt1jBBpX0_nsru-AUSMTk9wpNAEw",
-    authDomain: "bd-e-commerce-6716c.firebaseapp.com",
-    databaseURL: "https://bd-e-commerce-6716c-default-rtdb.firebaseio.com",
-    projectId: "bd-e-commerce-6716c",
-    storageBucket: "bd-e-commerce-6716c.appspot.com",
-    messagingSenderId: "1058664642229",
-    appId: "1:1058664642229:web:548a5264b37fbad407f0fc",
-    measurementId: "G-PJLLMF9PNR"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   
 const app = initializeApp(firebaseConfig);
